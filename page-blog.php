2551e4cbd39df67e86b8eeb516a78efdd2a54388
Sandra-beep <sandra.rivas@medieinstitutet.se>
2021-09-10 15:57:17 +0200

    löst det med loopen genom blogpost

--- page-blog.php
+++ page-blog.php
@@ -23,60 +23,46 @@ get_header();
 					<div class="row">
 						<div id="primary" class="col-xs-12 col-md-9">
 							<h1><?php the_title(); ?></h1>
-							<article>
-							<img src="<?php echo get_home_url();?>/wp-content/uploads/2021/09/washington.jpg" />
-								<h2 class="title">
-									<a href="inlagg.html">Det tredje inlägget</a>
-								</h2>
-								<ul class="meta">
-									<li>
-										<i class="fa fa-calendar"></i> 1 januari, 2016
-									</li>
-									<li>
-										<i class="fa fa-user"></i> <a href="forfattare.html">Peter Pärmenäs</a>
-									</li>
-									<li>
-										<i class="fa fa-tag"></i> <a href="kategori.html">Kategori 1</a>, <a href="kategori.html">Kategori 2</a>
-									</li>
+							<?php 
+								// the query
+								$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+								
+								<?php if ( $wpb_all_query->have_posts() ) : ?>
+								
+								<ul>
+								
+									<!-- the loop -->
+									<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+									<article>
+									
+										<img src="<?php echo get_the_post_thumbnail_url(); ?>" />
+											<h2 class="title">
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h2>
+											<ul class="meta">
+												<li>
+													<i class="fa fa-calendar"></i> 1 januari, 2016
+												</li>
+												<li>
+													<i class="fa fa-user"></i> <a href="forfattare.html">Peter Pärmenäs</a>
+												</li>
+												<li>
+													<i class="fa fa-tag"></i> <a href="kategori.html">Kategori 1</a>, <a href="kategori.html">Kategori 2</a>
+												</li>
+											</ul>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex. Phasellus tristique nunc in leo faucibus, a consequat nulla sagittis. In sed mi mi. Praesent condimentum sollicitudin nibh. Vivamus vulputate purus quis volutpat fringilla. Ut tortor libero, semper eget dolor vel, hendrerit tempus dui. Suspendisse dictum efficitur blandit. In porta scelerisque nulla ac placerat.</p>
+									</article>
+									<?php endwhile; ?>
+									<!-- end of the loop -->
+								
 								</ul>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex. Phasellus tristique nunc in leo faucibus, a consequat nulla sagittis. In sed mi mi. Praesent condimentum sollicitudin nibh. Vivamus vulputate purus quis volutpat fringilla. Ut tortor libero, semper eget dolor vel, hendrerit tempus dui. Suspendisse dictum efficitur blandit. In porta scelerisque nulla ac placerat.</p>
-							</article>
-							<article>
-								<img src="<?php echo get_home_url();?>/wp-content/uploads/2021/09/paris.jpg" />
-								<h2 class="title">
-									<a href="inlagg.html">Mitt andra inlägg</a>
-								</h2>
-								<ul class="meta">
-									<li>
-										<i class="fa fa-calendar"></i> 1 januari, 2016
-									</li>
-									<li>
-										<i class="fa fa-user"></i> <a href="forfattare.html">John Doe</a>
-									</li>
-									<li>
-										<i class="fa fa-tag"></i> <a href="kategori.html">Kategori 1</a>, <a href="kategori.html">Kategori 3</a>
-									</li>
-								</ul>
-								<p>Integer quis eros at urna pulvinar mollis eget quis ex. Nam fringilla tellus at ligula consequat, sed sagittis ipsum ultricies. Nunc pretium bibendum enim id iaculis. Nam sed leo non sem aliquam imperdiet dictum vitae magna. Phasellus ac accumsan dolor, quis congue mauris. Maecenas vehicula, arcu sed congue euismod, mi urna rhoncus nunc, et feugiat arcu tellus vitae odio. Sed luctus vel lorem vel dictum. Suspendisse potenti. Proin vel efficitur ex. Suspendisse scelerisque rutrum mattis. In sed laoreet augue, non maximus ante. Nam a rhoncus ligula. Aliquam at aliquet lorem. Suspendisse non risus tincidunt, gravida augue non, fringilla libero.</p>
-							</article>
-							<article>
-								<img src="<?php echo get_home_url();?>/wp-content/uploads/2021/09/london.jpg" />
-								<h2 class="title">
-									<a href="inlagg.html">Hej världen!</a>
-								</h2>
-								<ul class="meta">
-									<li>
-										<i class="fa fa-calendar"></i> 1 januari, 2016
-									</li>
-									<li>
-										<i class="fa fa-user"></i> <a href="forfattare.html">Peter Pärmenäs</a>
-									</li>
-									<li>
-										<i class="fa fa-tag"></i> <a href="kategori.html">Kategori 1</a>, <a href="kategori.html">Kategori 2</a>, <a href="kategori.html">Kategori 3</a>
-									</li>
-								</ul>
-								<p>Sed pretium metus in urna vehicula imperdiet. Fusce at odio vitae felis maximus tincidunt. Nunc porttitor ex a elementum dictum. Maecenas eget arcu nulla. Cras auctor dui aliquet, condimentum orci vel, venenatis nisl. Ut semper magna urna, ac congue dui cursus vitae. Nam sed pharetra leo, vel tincidunt est. Phasellus volutpat tortor nec nulla feugiat congue. Donec quis ligula varius, euismod nisl eu, aliquet metus. Aliquam tempus iaculis odio, sed volutpat mi aliquam aliquet.</p>
-							</article>
+								
+									<?php wp_reset_postdata(); ?>
+								
+								<?php else : ?>
+									<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+								<?php endif; ?>
+							
 							<nav class="navigation pagination">
 								<h2 class="screen-reader-text">Inläggsnavigering</h2>
 								<a class="prev page-numbers" href="">Föregående</a>
--- page-undersida-4.php
+++ page-undersida-4.php
@@ -23,9 +23,7 @@ get_header();
 					<div class="row">
 						<div class="col-xs-12 col-sm-8 col-md-6">
                         <h1><?php the_title(); ?></h1>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex. Phasellus tristique nunc in leo faucibus, a consequat nulla sagittis. In sed mi mi. Praesent condimentum sollicitudin nibh. Vivamus vulputate purus quis volutpat fringilla. Ut tortor libero, semper eget dolor vel, hendrerit tempus dui. Suspendisse dictum efficitur blandit. In porta scelerisque nulla ac placerat.</p>
-							<p>Integer quis eros at urna pulvinar mollis eget quis ex. Nam fringilla tellus at ligula consequat, sed sagittis ipsum ultricies. Nunc pretium bibendum enim id iaculis. Nam sed leo non sem aliquam imperdiet dictum vitae magna. Phasellus ac accumsan dolor, quis congue mauris. Maecenas vehicula, arcu sed congue euismod, mi urna rhoncus nunc, et feugiat arcu tellus vitae odio. Sed luctus vel lorem vel dictum. Suspendisse potenti. Proin vel efficitur ex. Suspendisse scelerisque rutrum mattis. In sed laoreet augue, non maximus ante. Nam a rhoncus ligula. Aliquam at aliquet lorem. Suspendisse non risus tincidunt, gravida augue non, fringilla libero.</p>
-							<p>Sed pretium metus in urna vehicula imperdiet. Fusce at odio vitae felis maximus tincidunt. Nunc porttitor ex a elementum dictum. Maecenas eget arcu nulla. Cras auctor dui aliquet, condimentum orci vel, venenatis nisl. Ut semper magna urna, ac congue dui cursus vitae. Nam sed pharetra leo, vel tincidunt est. Phasellus volutpat tortor nec nulla feugiat congue. Donec quis ligula varius, euismod nisl eu, aliquet metus. Aliquam tempus iaculis odio, sed volutpat mi aliquam aliquet.</p>
+							<p><?php the_content(); ?></p>
 						</div>
 						<div class="col-xs-12 col-sm-4 col-md-6">
 							<img src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/09/photo.jpg" />
